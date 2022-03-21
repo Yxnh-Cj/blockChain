8cee153e62cf1540f94c2a25867c8fe64f415149
Yxnh-Cj <93480778+Yxnh-Cj@users.noreply.github.com>
2022-03-21 19:26:39 +0800

    自动打卡

--- python_files/files/QQ空间.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import re
-info = input('输入QQ号:')
-res = requests.get(f"https://user.qzone.qq.com/{info}/infocenter/?_t_=0.6244913654137765")
-soup = BeautifulSoup(res.text,'lxml')
-response = soup.prettify()
-res = re.findall('<.*>',response)
-print(res)
--- python_files/files/百度地图调用.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-address = input('请输入地点:')
-par = {'address':address,'key':'cb6492a25c1451adbeca73623251'}
-url = 'http://api.map.baidu.com/reverse_geocoding/v3/?ak=Qk9nulzKvkYssHoPV9fyAEVQlqb3DkLn&output=json&coordtype=wgs84ll&location=31.225696,121.49884'
-res = requests.get(url,par)
-response = eval(res.text)
-print(response['result']['location']['lng'],response['result']['location']['lat'])
